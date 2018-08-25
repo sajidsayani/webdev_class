065aa6b8a846269625cd59c0939d07ce5c276e02
sajidsayani <androidsay@gmail.com>
2018-08-25 23:12:01 +0500

    added function search_businesses

--- my_yelp.py
+++ my_yelp.py
@@ -2,29 +2,26 @@
 import requests
 
 #declaring function
-def search_business();
+def search_businesses():
 
+    api_key="TZtNk0uDCSOKB1rUr1crScByluu0WVdy10Hfozbn7OAnWsAmkYc5Bdah0r_ggXH5SS3mobO_o2kl4ZB7wJMH07LA6uoKyOgEgYtdFBWzlbgPkM45z7CC44Yy62t_W3Yx"
 
+    url="https://api.yelp.com/v3/businesses/search"
 
-api_key="TZtNk0uDCSOKB1rUr1crScByluu0WVdy10Hfozbn7OAnWsAmkYc5Bdah0r_ggXH5SS3mobO_o2kl4ZB7wJMH07LA6uoKyOgEgYtdFBWzlbgPkM45z7CC44Yy62t_W3Yx"
+    my_headers ={
+        "Authorization": "Bearer %s" % api_key
+    }
 
-url="https://api.yelp.com/v3/businesses/search"
+    my_params = {
+    "term": "restaurants",
+    "location": "chicago",
+    "limit":3,
+    }
 
-my_headers ={
-    "Authorization": "Bearer %s" % api_key
-}
+    businesses_object = requests.get(url, headers=my_headers, params=my_params)
 
-my_params = {
-"term": "restaurants",
-"location": "chicago",
-"limit":3,
-}
-
-businesses_object = requests.get(url, headers=my_headers, params=my_params)
-
-businesses_dict = businesses_object.text
-print(businesses_dict)
-
+    businesses_dict = businesses_object.text
+    print(businesses_dict)
 
 #calling function
-search_business()
+search_businesses()

2d6dc39f649b96f664d8bc12771d7061da4bfe03
sajidsayani <androidsay@gmail.com>
2018-08-25 23:15:28 +0500

    added search parmaeters to the function and hardcoded values

--- my_yelp.py
+++ my_yelp.py
@@ -2,7 +2,7 @@
 import requests
 
 #declaring function
-def search_businesses():
+def search_businesses(search_term, search_location):
 
     api_key="TZtNk0uDCSOKB1rUr1crScByluu0WVdy10Hfozbn7OAnWsAmkYc5Bdah0r_ggXH5SS3mobO_o2kl4ZB7wJMH07LA6uoKyOgEgYtdFBWzlbgPkM45z7CC44Yy62t_W3Yx"
 
@@ -24,4 +24,4 @@ def search_businesses():
     print(businesses_dict)
 
 #calling function
-search_businesses()
+search_businesses("restaurants","chicago")

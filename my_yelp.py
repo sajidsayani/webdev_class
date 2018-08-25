4c665a505dfa6107cfbb3b1a637ca23585d35f8a
sajidsayani <androidsay@gmail.com>
2018-08-25 23:08:27 +0500

    working rest api call

--- my_yelp.py
+++ my_yelp.py
@@ -1,4 +1,10 @@
+
 import requests
+
+#declaring function
+def search_business();
+
+
 
 api_key="TZtNk0uDCSOKB1rUr1crScByluu0WVdy10Hfozbn7OAnWsAmkYc5Bdah0r_ggXH5SS3mobO_o2kl4ZB7wJMH07LA6uoKyOgEgYtdFBWzlbgPkM45z7CC44Yy62t_W3Yx"
 
@@ -18,3 +24,7 @@ businesses_object = requests.get(url, headers=my_headers, params=my_params)
 
 businesses_dict = businesses_object.text
 print(businesses_dict)
+
+
+#calling function
+search_business()
